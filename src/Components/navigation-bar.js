504451d7b007aa0ae040bc4f0594b35a0c469d2f
ShahzerBaig <shahzer.baig@gmail.com>
2021-09-09 15:06:12 +0530

    Added navigation Bar.js in Component Folder

---
+++ src/Components/navigation-bar.js
@@ -0,0 +1,20 @@
+import './styles/navigation-bar.css'
+
+function NavigationBar(){
+    return(
+        <div className="nav-bar">
+            <nav className="menu">
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Contact Us</li>
+
+                </ul>
+            </nav>
+
+        </div>
+    );
+
+}
+
+export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Contactus from './Components/contact-us'
+import Contactus from './Components/contact-us';
+import NavigationBar from './Components/navigation-bar'
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavigationBar/>
     <Contactus />
   </React.StrictMode>,
   document.getElementById('root')
